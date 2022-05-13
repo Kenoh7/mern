a90e3f6ea206b586dfaad289aab2e95d06af8fb1
Kenoh7 <97468788+Kenoh7@users.noreply.github.com>
2022-05-12 22:28:51 -0700

    mern

--- product_manager_part1/server/routes/pm.routes.js
+++
@@ -1,9 +0,0 @@
-const PmController = require("../controllers/pm.controllers");
-
-module.exports = (app) => {
-    app.get("/api/pm/", PmController.findAllData);
-    app.get("/api/pm/:id", PmController.findOneSingleData);
-    app.put("/api/pm/:id", PmController.updateExistingData);
-    app.post("/api/pm/new", PmController.createNewData);
-    app.delete("/api/pm/delete/:id", PmController.deleteAnExistingData);
-};
