0fbaac3c02c2bd61beab69b6fb101fa6eaebd7e1
Kenoh7 <97468788+Kenoh7@users.noreply.github.com>
2022-05-12 22:25:55 -0700

    mern

--- authors/server/routes/author.routes.js
+++
@@ -1,9 +0,0 @@
-const AuthorController = require("../controllers/author.controllers");
-
-module.exports = (app) => {
-    app.get("/api/author/", AuthorController.findAllData);
-    app.get("/api/author/:id", AuthorController.findOneSingleData);
-    app.put("/api/author/:id", AuthorController.updateExistingData);
-    app.post("/api/author/new", AuthorController.createNewData);
-    app.delete("/api/author/delete/:id", AuthorController.deleteAnExistingData);
-}
